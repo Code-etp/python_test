9337923fabe01a490fe4e69ba089b04a51a8d628
abdul-lateefolawole <abdul-lateef.alabi@deelaa.com>
2025-01-12 14:04:08 +0100

    python-test

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from github import Github
 
 
 # Set up authentication
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_TOKEN = "${{secret.TOKEN}}"
 ORG_NAME = "lahteeph"
 WORKFLOW_PATH = ".github/workflows/"
 SEARCH_STRING = "aws-actions/amazon-ecs-deploy-task-definition@v1"
